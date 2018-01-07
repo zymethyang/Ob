67129f46815e8a7826d610cea36c81a5bd58cfd6
Nguyen Minh Tri <zymethyang@gmail.com>
2018-01-07 20:59:56 +0700

    Initial

--- src/app.js
+++ src/app.js
@@ -2,3 +2,18 @@ import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
 console.log('RxJS Boiler Running...');
+
+const btn = $('.btn');
+const btnStream = Rx.Observable.fromEvent(btn,'click');
+
+btnStream.subscribe(
+    e =>{
+        console.log('Clicked');
+    },
+    err =>{
+        console.log(err);
+    },
+    ()=>{
+        console.log('Completed');
+    }
+);
